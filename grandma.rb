2e9790e8e865f42e050d37dbf5c5fbfc2b3154c3
Phoebe Lau <phoebelau7@gmail.com>
2017-11-27 18:19:00 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,12 +13,12 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase == phrase.downcase
-    "HUH?! SPEAK UP, SONNY!"
-    speak_to_grandma(phrase)
-  elsif phrase == "I LOVE YOU GRANDMA!"
+  if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
     "NO, NOT SINCE 1938!"
+  elsif phrase == phrase.downcase
+      "HUH?! SPEAK UP, SONNY!"
+      speak_to_grandma(phrase)
   end
 end
