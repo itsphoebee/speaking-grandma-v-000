5e08c5e4e6d87e0034a82b68598d55328e6c089b
Phoebe Lau <phoebelau7@gmail.com>
2017-11-27 18:19:10 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ def speak_to_grandma(phrase)
     "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
     "NO, NOT SINCE 1938!"
-  elsif phrase == phrase.downcase
+  else
       "HUH?! SPEAK UP, SONNY!"
       speak_to_grandma(phrase)
   end
